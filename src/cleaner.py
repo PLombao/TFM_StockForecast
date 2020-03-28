221bb553f16eabca272327e9171c8c67fc555927
PLombao <pablombao@gmail.com>
2020-03-28 14:58:52 +0100

    add full stock data

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,37 +1,156 @@
+import numpy as np
+import pandas as pd
+
+from src.utils import get_agg_stats
+
+def clean_csv(df, datecol):
+    print('{:=^40}'.format('  CLEAN CSV  '.format()))
+    # Correct columns for all lowcase
+    df.columns = [col.lower() for col in df.columns]
+    print("Set columns to lowcase.")
+
+    # Sort by date
+    df = df.sort_values(datecol)
+    print("Sort values by date.")
+
+    # Drop duplicates
+    filter_data = df.drop_duplicates()
+    print("Dropped duplicates. Rows dropped: {}."\
+        .format(df.shape[0]-filter_data.shape[0]))
+
+    print('{:=^40}'.format(''.format()))
+    return filter_data
 
 def clean_ventas(data):
-    print("DATA CLEANING VENTAS")
+    print('{:=^40}'.format('  CLEAN VENTAS  '.format()))
+
+    if data.shape[0] != data[['fecha',"producto"]].drop_duplicates().shape[0]:
+        print("[WARNING] Ventas data with different units for same product & data. Rows: {}"\
+            .format(data.shape[0] - data[['fecha',"producto"]].drop_duplicates().shape[0]))
 
     # Pasamos a enteros las unidades truncando
     data.udsventa = data.udsventa.apply(lambda x: int(x))
-    print(" - Pasadas unidades venta a enteros.")
+    print("UnidadesVentas to integer.")
 
     # Eliminamos ceros (no se especifican todos los ceros)
     filter_data = data.loc[data.udsventa != 0].reset_index(drop=True)
-    print(" - Eliminados registros con cero en las unidades venta. Registros eliminados: {}"\
+    print("Drop rows with zero in UnidadesVenta.")
+    print("  Rows dropped: {}"\
         .format(data.shape[0]-filter_data.shape[0]))
 
+    print('{:=^40}'.format(''.format()))
     return filter_data
 
 def clean_promos(data):
+    print('{:=^40}'.format('  CLEAN PROMOS  '.format()))
     # Eliminamos promociones sin fecha final
     filter_data = data.loc[~data.finpromo.isna()].reset_index(drop=True)
-    print(" - Eliminados registros con null en fecha final de promocion. Registros eliminados: {}"\
+    print("Drop rows of promos without end date:")
+    print("   Rows dropped: {}"\
         .format(data.shape[0]-filter_data.shape[0]))
-    print("   La fecha más reciente de inicio de promocion de los eliminados es {}"\
+    print("   The most recent of the promos dropped was started in {}"\
         .format(data.loc[data.finpromo.isna(), "iniciopromo"].max()))
-
+    print('{:=^40}'.format(''.format()))
     return filter_data
 
 def clean_stock(data):
+    print('{:=^40}'.format('  CLEAN STOCK  '.format()))
+
+    if data.shape[0] != data[['fecha',"producto"]].drop_duplicates().shape[0]:
+        print("[WARNING] Stock data with different units for same product & data. Rows: {}"\
+            .format(data.shape[0] - data[['fecha',"producto"]].drop_duplicates().shape[0]))
+
+    print('{:=^40}'.format(''.format()))
     return data
 
 def clean_prevision(data):
+    print('{:=^40}'.format('  CLEAN PREVISION  '.format()))
+    if data.shape[0] != data[['fecha',"producto"]].drop_duplicates().shape[0]:
+        print("[WARNING] Ventas data with different units for same product & data. Rows: {}"\
+            .format(data.shape[0] - data[['fecha',"producto"]].drop_duplicates().shape[0]))
+
     data.udsprevisionempresa = data.udsprevisionempresa.apply(lambda x: int(x))
-    print(" - Pasadas unidades prevision de venta a enteros.")
+    print("UnidadesPrevisionVentas to integer.")
+    print('{:=^40}'.format(''.format()))
+    return data
+
+def clean_festivos(data):
+    print('{:=^40}'.format('  CLEAN FESTIVOS  '.format()))
+    data = data.loc[:,['fecha', 'festivo']]
+    data.festivo = data.festivo.apply(lambda x: 1)
+    print("Drop unnecesary columns.")
+    print('{:=^40}'.format(''.format()))
 
     return data
 
-def clean_data(data):
+def build_ventas_byproduct(ventas):
+    print('{:=^60}'.format('  BUILD RANGED PROMOS  '))
+    venta_stats, venta_names = get_agg_stats("venta")
 
-    return data
+    ventas_byprod = ventas.groupby("producto").agg({"fecha":["min","max","count"],
+                                             "udsventa": venta_stats})
+    ventas_byprod = ventas_byprod.reset_index()
+
+    ventas_byprod.columns = ["producto", "fecha_primera_venta", "fecha_ultima_venta","freq_venta"] + venta_names
+    print("Dataset ventas by product builded")
+    print('{:=^60}'.format(''))
+    return ventas_byprod
+
+def build_promos_ranged(promos):
+    print('{:=^60}'.format('  BUILD RANGED PROMOS  '))
+    # Nos quedamos con las cols utiles y eliminamos promos repetidas
+    promos = promos[["iniciopromo","finpromo","producto"]].drop_duplicates()
+
+    # Inicializamos promos range
+    promos_rng = pd.DataFrame({})
+
+    for promo in promos.values:
+        # Añadimos una promocion como rango
+        dates = pd.date_range(promo[0], promo[1], freq='D')
+        df_promo = pd.DataFrame({"fecha": dates,
+                               "producto": promo[2]})
+        promos_rng=pd.concat([promos_rng, df_promo])
+
+    # Eliminamos promociones repetidas y añadimos flag de promo
+    promos_rng = promos_rng.drop_duplicates()
+    promos_rng["promo"] = 1
+
+    print("Dataset promos from intervals to range builded.")
+    print('{:=^60}'.format(''))
+    print("")
+    return promos_rng.reset_index(drop=True)
+
+
+def join_data(stock, ventas, prevision, promos_rng, festivos):
+    print('{:=^60}'.format('  JOIN DATASET STOCK  '))
+    # TEMPORARY: grouping stock by product and date
+    stock = stock.groupby(["fecha","producto"]).agg(lambda x: int(x.mean())).reset_index()
+    print("[WARNING] Dropping duplicates in fecha | producto for stock data [TEMPORARY]")
+    print("Input shape: {}".format(stock.shape))
+
+    # Convert date to str for the join
+    stock["idfecha"] = stock.fecha.apply(lambda x: x.strftime('%Y%m%d'))
+
+    # Joining date | product
+    for df in [ventas, prevision, promos_rng]:
+        df["idfecha"] = df.fecha.apply(lambda x: x.strftime('%Y%m%d'))
+        df = df.drop("fecha", axis=1)
+        stock = stock.merge(df, on=['idfecha', 'producto'], how='left')
+    
+    # Joining date
+    for df in [festivos]:
+        df["idfecha"] = df.fecha.apply(lambda x: x.strftime('%Y%m%d'))
+        df = df.drop("fecha", axis=1)
+        stock = stock.merge(df, on=['idfecha'], how='left')
+
+    # Assign missings to 0 in promo & festivo
+    stock["promo"] = stock["promo"].fillna(0)
+    stock["festivo"] = stock["festivo"].fillna(0)
+
+    # Drop id fecha
+    stock = stock.drop("idfecha", axis=1)
+    
+    # stock = stock.drop("idfecha")
+    print("Output shape: {}".format(stock.shape))
+    print('{:=^60}'.format(''))
+    return stock
--- src/load_data.py
+++ src/load_data.py
@@ -1,29 +1,28 @@
 import pandas as pd
 import numpy as np
 from src.read_config import read_source_data
-from src.utils import get_agg_stats
-from src.cleaner import clean_ventas, clean_promos, clean_stock, clean_prevision
+from src.cleaner import clean_csv, clean_ventas, clean_promos, clean_stock, clean_prevision, clean_festivos
+from src.cleaner import build_ventas_byproduct, build_promos_ranged, join_data
 
 def load_csv(dataset):
-    print(" - Loading dataset {}...".format(dataset))
+    print('{:=^60}'.format('  LOAD DATASET {} '.format(dataset)))
+
     # Read dataset info
     [filename, dates, columns, shape] = read_source_data(dataset)
 
     # Load csv
     path = "data/" + filename
+    print("Reading CSV in {}...".format(path))
     df = pd.read_csv(path, sep=";", decimal = ",", encoding='latin-1',
                     parse_dates=dates, dtype=columns)
 
-    # Correct columns for all lowcase
-    df.columns = [col.lower() for col in df.columns]
-
-    # Sort by date
-    df = df.sort_values(dates[0])
-
-    # Check shape of dataframe
+    # Check shape of dataframe file
     if df.shape != shape:
         print("[WARNING] Check out shape of dataset {}.".format(dataset))
         print("[WARNING] Should be {} and instead is {}.".format(shape, df.shape))
+
+    # Clean csv
+    df = clean_csv(df, dates[0])
 
     # Clean datasets
     if dataset == "ventas": 
@@ -34,68 +33,56 @@ def load_csv(dataset):
         df = clean_stock(df)
     elif dataset == "prevision":
         df = clean_prevision(df)
+    elif dataset == "festivos":
+        df = clean_festivos(df)
     else:
         print("[WARNING] Cleaner not configured in load data for dataset {}.".format(dataset))
 
 
-    print("     Dataset {} loaded.".format(dataset))
-    return df
+    print("Dataset {} loaded. Shape: {}".format(dataset, df.shape))
+    print('{:=^60}'.format(''))
+    print("")
+    return df.reset_index(drop=True)
 
 def load_ventas_byproduct(ventas=None):
     if type(ventas) == type(None):
         ventas = load_csv("ventas")
-    print(" - Building dataset ventas by product")
-    venta_stats, venta_names = get_agg_stats("venta")
 
-    ventas_byprod = ventas.groupby("producto").agg({"fecha":["min","max","count"],
-                                             "udsventa": venta_stats})
-    ventas_byprod = ventas_byprod.reset_index()
-
-    ventas_byprod.columns = ["producto", "fecha_primera_venta", "fecha_ultima_venta","freq_venta"] + venta_names
-    print("    Dataset ventas by product builded")
+    ventas_byprod = build_ventas_byproduct(ventas)
     return ventas_byprod
 
 def load_promos_range(promos=None):
-    print(" - Building dataset promos by range")
     if type(promos) == type(None):
         promos = load_csv("promos")
-    # Nos quedamos con las cols utiles y eliminamos promos repetidas
-    promos = promos[["iniciopromo","finpromo","producto"]].drop_duplicates()
-
-    # Inicializamos promos range
-    promos_rng = pd.DataFrame({})
-
-    for promo in promos.values:
-        # Añadimos una promocion como rango
-        dates = pd.date_range(promo[0], promo[1], freq='D')
-        df_promo = pd.DataFrame({"fecha": dates,
-                               "producto": promo[2]})
-        promos_rng=pd.concat([promos_rng, df_promo])
-
-    # Eliminamos promociones repetidas y añadimos flag de promo
-    promos_rng = promos_rng.drop_duplicates()
-    promos_rng["promo"] = 1
-
-    print("    Dataset promos by range builded")  
-    return promos_rng.reset_index(drop=True)
+    
+    promos_rng = build_promos_ranged(promos)
+    return promos_rng
 
 def load_data():
     """
     Loads data from src
     """
-    ventas = load_csv("ventas")
-    promos = load_csv("promos")
     stock = load_csv("stock")
+    ventas = load_csv("ventas")
     prevision = load_csv("prevision")
     festivos = load_csv("festivos")
-    ventas_byprod = load_ventas_byproduct(ventas)
-    promos_rng = load_promos_range(promos)
-    
-    return ventas, promos, stock, prevision, festivos, ventas_byprod, promos_rng
+    promos_rng = load_promos_range()
+
+    stock = join_data(stock, ventas, prevision, promos_rng, festivos)
+
+    return stock
 
 if __name__ == "__main__":
     print("TESTING LOAD DATA")
-    ventas, promos, stock, prevision, festivos, \
-        ventas_byprod,  promos_rng = load_data()
+
+    stock = load_data()
+
+    # # print(stock.info())
+    print(stock.head())
+
+
+    # print("Duplicates")
+    # print(stock.shape)
+    # print(stock.drop_duplicates().shape)
 
 
