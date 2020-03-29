cb600b2ede7992da8f8cbf659f27690de6a4b42b
PLombao <pablombao@gmail.com>
2020-03-29 16:57:01 +0200

    add json config for models

--- src/read_config.py
+++ src/read_config.py
@@ -26,3 +26,30 @@ def read_source_data(dataset):
         shape = (rows, cols)
 
         return [filename, dates, columns, shape]
+
+
+def read_config_model(modelo, name):
+    """
+    Read config.json file
+    
+    Args:
+        modelo (str):   two possible values: "stock" or "clustering"
+        name (str):     model name in the json
+    Returns:
+        list_var_model (list):          a list with the variables which use the model
+        dict_params_model (dictionary): a dictionary with the params of the model (base_learner)
+        dict_tags_model (dictionary):   a dictionary with the tags of the model (to mlflow)
+    """
+    if modelo in ["stock", "clustering"]:
+        json_path = "./config/model_" + modelo + ".json"
+
+        with open(json_path) as config_file: 
+            config = json.load(config_file)
+            info_model_dict = config[name]
+            variables = info_model_dict['variables']
+            tags = info_model_dict['tags']
+            params = info_model_dict['params']
+        
+        return variables, tags, params
+    else:
+        print("[ERROR] File ./config/model_" + modelo + ".json doesn't exist")
