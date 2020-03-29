753c79097c4ba66005e871649a6af6609ea4b319
PLombao <pablombao@gmail.com>
2020-03-29 21:06:17 +0200

    rebuild entirely join function

--- src/builder.py
+++ src/builder.py
@@ -40,37 +40,43 @@ def build_promos_ranged(promos):
     print("")
     return promos_rng.reset_index(drop=True)
 
-
 def join_data(stock, ventas, prevision, promos_rng, festivos):
     print('{:=^60}'.format('  JOIN DATASET STOCK  '))
+    print("Input shape: {}".format(stock.shape))
+
     # TEMPORARY: grouping stock by product and date
     stock = stock.groupby(["fecha","producto"]).agg(lambda x: int(x.mean())).reset_index()
     print("[WARNING] Dropping duplicates in fecha | producto for stock data [TEMPORARY]")
-    print("Input shape: {}".format(stock.shape))
 
-    # Convert date to str for the join
-    stock["idfecha"] = stock.fecha.apply(lambda x: x.strftime('%Y%m%d'))
-
-    # Joining date | product
-    for df in [ventas, prevision, promos_rng]:
-        df["idfecha"] = df.fecha.apply(lambda x: x.strftime('%Y%m%d'))
-        df = df.drop("fecha", axis=1)
-        stock = stock.merge(df, on=['idfecha', 'producto'], how='left')
+    fecha_ini = stock.fecha.min().date()
+    fecha_fin = stock.fecha.max().date()
+    print("Stock dataframe received with data from {} to {}".format(fecha_ini, fecha_fin))
     
-    # Joining date
-    for df in [festivos]:
-        df["idfecha"] = df.fecha.apply(lambda x: x.strftime('%Y%m%d'))
-        df = df.drop("fecha", axis=1)
-        stock = stock.merge(df, on=['idfecha'], how='left')
+    # Creamos un df con todas las combinaciones de fechas y productos del dataset stock
+    total_dates = pd.DataFrame({"fecha": pd.date_range(fecha_ini, fecha_fin, freq='D'),
+                                "key":1})
+    total_products = pd.DataFrame({"producto": stock.producto.sort_values().unique(),
+                                   "key":1})
+    total = total_dates.merge(total_products, on='key').drop("key", axis=1)
+    print("Created dataframe for {} days and {} products [shape: {}]"\
+        .format(total_dates.shape[0],total_products.shape[0], total.shape))
+
+    # Hacemos un merge con los datos de stock, ventas, prevision y promos range
+    for df, name in zip([stock, ventas, prevision, promos_rng], ['stock', 'ventas','prevision', "promos range"]):
+        total = total.merge(df, on=['fecha', 'producto'], how='left')
+        print("Merged dataframe with {} data    [shape: {}]".format(name, total.shape))
+
+    # Hacemos un merge con los datos de festivos
+    total = total.merge(festivos, on=['fecha'], how='left')
+    print("Merged dataframe with festivos data    [shape: {}]".format(total.shape))
 
     # Assign missings to 0 in promo & festivo
-    stock["promo"] = stock["promo"].fillna(0)
-    stock["festivo"] = stock["festivo"].fillna(0)
+    total["promo"] = total["promo"].fillna(0)
+    total["festivo"] = total["festivo"].fillna(0)
 
-    # Drop id fecha
-    stock = stock.drop("idfecha", axis=1)
-    
-    # stock = stock.drop("idfecha")
-    print("Output shape: {}".format(stock.shape))
+    # Add new columns weekday
+    total['weekday'] = total['fecha'].apply(lambda x: x.weekday())
+
+    print("Output shape: {}".format(total.shape))
     print('{:=^60}'.format(''))
-    return stock
+    return total
